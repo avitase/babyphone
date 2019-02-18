98826ca974b3271b5a7b1cde33367aa3dad594e5
Nis Meinert <nis.meinert@gmail.com>
2019-02-18 10:48:25 +0100

    move jpgs to img/

--- camera.py
+++ camera.py
@@ -1,3 +1,4 @@
+import os
 import time
 
 from base_camera import BaseCamera
@@ -6,7 +7,8 @@ from base_camera import BaseCamera
 class Camera(BaseCamera):
     """An emulated camera implementation that streams a repeated sequence of
     files 1.jpg, 2.jpg and 3.jpg at a rate of one frame per second."""
-    imgs = [open(f + '.jpg', 'rb').read() for f in ['1', '2', '3']]
+    file_names = [os.path.join('img', f) + '.jpg' for f in map(str, [1, 2, 3])]
+    imgs = [open(f, 'rb').read() for f in file_names]
 
     @staticmethod
     def frames():
