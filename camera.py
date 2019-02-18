f79ba01b0c7cf213f4e2fec65a9dbe844c92c164
Nis Meinert <nis.meinert@gmail.com>
2019-02-18 12:32:19 +0100

    remove comments

--- camera.py
+++ camera.py
@@ -5,8 +5,6 @@ from base_camera import BaseCamera
 
 
 class Camera(BaseCamera):
-    """An emulated camera implementation that streams a repeated sequence of
-    files 1.jpg, 2.jpg and 3.jpg at a rate of one frame per second."""
     file_names = [os.path.join('img', f) + '.jpg' for f in map(str, [1, 2, 3])]
     imgs = [open(f, 'rb').read() for f in file_names]
 
