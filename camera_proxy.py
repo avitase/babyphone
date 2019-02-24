40e2ba9fe2d95281f67017021d00b61e817926ac
Nis Meinert <mail@nismeinert.de>
2019-02-24 21:36:48 +0100

    add text overlay with timestamp

--- camera_proxy.py
+++ camera_proxy.py
@@ -1,6 +1,8 @@
-import time
+import io
 
 import zmq
+from PIL import Image, ImageFont, ImageDraw
+import datetime as dt
 
 
 class CameraProxy(object):
@@ -8,9 +10,17 @@ class CameraProxy(object):
         self._context = zmq.Context()
         self._socket = self._context.socket(zmq.REQ)
         self._socket.connect(socket)
-        self._last_update = time.time()
+        self._font = ImageFont.truetype("/usr/share/fonts/dejavu/DejaVuSans.ttf", 16)
 
     def get_frame(self):
         self._socket.send_string('')
-        self._last_update = time.time()
-        return self._socket.recv()
+
+        timestamp = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+        img = Image.open(io.BytesIO(self._socket.recv()))
+        draw = ImageDraw.Draw(img)
+        draw.text((10, 10), timestamp, (255, 255, 255), font=self._font)
+
+        raw = io.BytesIO()
+        img.save(raw, format='jpeg')
+        return raw.getvalue()
