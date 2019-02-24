a68c06df9ea5f29e83596de89af76162b8620cc4
Nis Meinert <mail@nismeinert.de>
2019-02-24 22:12:21 +0100

    add average FPS

--- camera_proxy.py
+++ camera_proxy.py
@@ -1,8 +1,10 @@
+import collections
+import datetime as dt
 import io
+import time
 
 import zmq
 from PIL import Image, ImageFont, ImageDraw
-import datetime as dt
 
 
 class CameraProxy(object):
@@ -10,16 +12,28 @@ class CameraProxy(object):
         self._context = zmq.Context()
         self._socket = self._context.socket(zmq.REQ)
         self._socket.connect(socket)
+
         self._font = ImageFont.truetype('/usr/share/fonts/dejavu/DejaVuSans.ttf', 16)
+
+        self.last_update = time.time()
+        self.fps = collections.deque(maxlen=50)
 
     def get_frame(self):
         self._socket.send_string('')
 
+        now = time.time
+        self.fps.append(1. / (now() - self.last_update))
+        self.last_update = now()
+
         timestamp = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        fps_avg = sum(self.fps) / len(self.fps)
 
         img = Image.open(io.BytesIO(self._socket.recv()))
         draw = ImageDraw.Draw(img)
-        draw.text((10, 10), timestamp, (255, 106, 0), font=self._font)
+        draw.text((10, 10),
+                  '{} ({:.0f} FPS)'.format(timestamp, fps_avg),
+                  (255, 106, 0),
+                  font=self._font)
 
         raw = io.BytesIO()
         img.save(raw, format='jpeg')
