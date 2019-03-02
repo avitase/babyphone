f648ff6351e6015a4e859bfbb2322635e4db8f38
Nis Meinert <mail@nismeinert.de>
2019-03-02 16:54:23 +0100

    slow down camera server

--- camera/base_camera.py
+++ camera/base_camera.py
@@ -1,8 +1,12 @@
+import time
+
 import zmq
 
 
 class BaseCamera(object):
     def __init__(self, socket):
+        self.MAX_FPS = 40
+
         context = zmq.Context()
         self._socket = context.socket(zmq.REP)
         self._socket.bind(socket)
@@ -13,6 +17,9 @@ class BaseCamera(object):
         self.is_running = False
 
     def run(self):
+        now = time.time
+        t = now()
+
         self.is_running = True
         while self.is_running:
             frame = self.next_frame()
@@ -25,6 +32,11 @@ class BaseCamera(object):
                     requests_left = False
                 else:
                     self._socket.send(frame)
+
+            dt = 1. / float(self.MAX_FPS) - (now() - t)
+            if dt > 0.:
+                time.sleep(dt)
+            t = now()
 
     def next_frame(self):
         raise RuntimeError('Must be implemented by subclasses.')
